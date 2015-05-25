b889122406403833ffb55d2e543f2a76e372dbd7
jostodafullest <jostodafullest@gmail.com>
2015-05-25 10:23:40 -0400

    Update get_key.pp

--- manifests/get_key.pp
+++ manifests/get_key.pp
@@ -1,12 +1,13 @@
 define serverbackup_cdp_agent::get_key {
     $server_filename = regsubst($name, '^(.*:)//([a-z0-9\-.]+)(:[0-9]+)?(.*)$','\2')
-    exec { "/usr/bin/serverbackup-setup --get-key ${name}":
+    exec { "/usr/bin/r1soft-setup --get-key ${name}":
         creates     => "/usr/sbin/r1soft/conf/server.allow/${server_filename}",
         require     => [
-            Package['serverbackup-enterprise-agent'],
+            Package['r1soft-cdp-enterprise-agent'],
             Exec['get-module'],
         ],
         logoutput   => true,
         notify      => Service['cdp-agent'],
     }
 }
+
