0920158daf8c60f20391118d0d95ced7134c7d84
jostodafullest <jostodafullest@gmail.com>
2015-05-25 10:25:07 -0400

    Update key.pp

--- manifests/key.pp
+++ manifests/key.pp
@@ -2,6 +2,6 @@ define serverbackup_cdp_agent::key {
     file { "/usr/sbin/r1soft/conf/server.allow/key":
         content => "${title}",
         notify      => Service['cdp-agent'],
-        require     => Package['serverbackup-enterprise-agent'],
+        require     => Package['r1soft-cdp-enterprise-agent'],
     }
 }
